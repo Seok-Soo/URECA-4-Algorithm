55654f4d19381aecd9d0a7805a055bee0f958827
Seok_Soo <whdtn7855@naver.com>
2026-04-23 13:42:29 +0900

    feat: 하노이탑 문제 풀이 추가

--- 백준/1914_하노이탑.java
+++ 백준/1914_하노이탑.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
+
 class Main{
+    static int count;
     public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int onepan = sc.nextInt();
+
+        hanoi(onepan, 1 ,2,3 );
         
 
         
     }//psvm
+
+    public static void hanoi(int n, int left, int mid, int right){
+
+        if(n == 1){
+            System.out.println(left + " " + right);
+            count++;
+            return;
+        }
+        
+        hanoi(n-1, left, right, mid);
+        System.out.println(left + " " + right);
+        count++;
+        hanoi(n-1, mid, left, right);
+        
+
+        
+        return;
+    }
 }//Main
