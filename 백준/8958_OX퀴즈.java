3a55d43d806cd104b5ff0ba7cf616a5b6578b52c
Seok_Soo <whdtn7855@naver.com>
2026-04-25 23:22:50 +0900

    feat: 8958_OX퀴즈 문제 풀이 추가

---
+++ 백준/8958_OX퀴즈.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main{
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bf.readLine());
+        
+        for (int i = 0; i < n; i++){
+            String s = bf.readLine();    
+            int count = 0;
+            int sum = 0;
+            for (int j = 0; j < s.length(); j++){
+                if(s.charAt(j) == 'O'){
+                    sum += ++count;
+                }else count = 0;
+
+            }
+            
+            System.out.println(sum);
+        }
+
+
+    }
+}
