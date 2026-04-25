19c19c0f268514695f800a33bb49e0e3a710a556
Seok_Soo <whdtn7855@naver.com>
2026-04-25 23:06:19 +0900

    feat: 11720_숫자의 합 문제 풀이 추가

---
+++ 백준/브론즈/11720_숫자의_합.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(bf.readLine(), "");
+
+        int n = Integer.parseInt(bf.readLine());
+        
+        String s = bf.readLine();
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            
+            sum += (s.charAt(i) - '0'); 
+
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(sum);
+        System.out.println(sb);
+    }
+    
+}
