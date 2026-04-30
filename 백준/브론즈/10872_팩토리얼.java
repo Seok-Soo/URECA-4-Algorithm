65683f6b0a2d5480fb6504ea912491feb0b342a3
Seok_Soo <whdtn7855@naver.com>
2026-04-30 09:45:26 +0900

    feat: 10872_팩토리얼 문제 풀이 추가

---
+++ 백준/브론즈/10872_팩토리얼.java
@@ -0,0 +1,28 @@
+package 브론즈;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+    
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        sb.append(factorial(n));
+        
+        System.out.print(sb);
+    }
+
+    private static int factorial(int n){
+        
+        if ( n <= 1)
+            return 1;
+        
+        return n * factorial(n - 1);
+
+    }
+
+}
