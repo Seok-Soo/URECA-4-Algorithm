dae2bb7f88f35c425dc0ab65da20642520384ea6
Seok_Soo <whdtn7855@naver.com>
2026-05-06 13:41:42 +0900

    feat: 1074_z 문제 풀이 추가

---
+++ 백준/1074_z.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+    static int r,c;
+    static int count = 0;
+
+    static void z(int x, int y, int size){
+        
+        if(size == 1){
+            return;
+        }
+
+        int half = size / 2;
+
+        if( r < x + half && c < y + half){
+            z(x, y, half);
+        }
+        else if( r < x + half && c >= y + half){
+            count += half * half;
+            z(x, y + half, half);
+        }
+        else if( r >= x + half && c < y + half){
+            count += 2 * half * half;
+            z(x + half, y, half);
+        }
+        else{
+            count += 3 * half * half;
+            z(x + half, y + half, half);
+        }
+
+    }//z
+    
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        
+        z(0, 0 ,(int)Math.pow(2,n));
+
+        System.out.println(count);
+
+    }
+}
