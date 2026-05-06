d35da6ca4d161fb7a787ed7f93fbb276475f227b
Seok_Soo <whdtn7855@naver.com>
2026-05-06 09:39:06 +0900

    feat: 2630_색종이만들기 문제 풀이 추가

---
+++ 백준/실버/2630_색종이만들기.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+
+    static int paper[][];
+    static int white, blue;
+
+    static void cut(int x, int y, int size){
+        int color = paper[x][y];
+        boolean same = true;
+        for( int i = x; i < size -1; i++){
+            for(int j = y; j < size-1; j++){
+                if(paper[i][j] != color){
+                    same = false;
+                    break;
+                }//if
+            }//for
+        }//for
+
+        if (same == true){
+            if (color == 0)
+                white++;
+            else
+                blue++;
+            return;
+        }
+
+        int newsize = size / 2;
+
+        cut(x,y,newsize);
+        cut(x, y + newsize, newsize);
+        cut(x + newsize, y, newsize);
+        cut(x + newsize, y + newsize, newsize);
+
+
+    }//cut
+
+    public static void main(String[] args) throws Exception{
+        
+        white = 0;
+        blue = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        paper = new int[n][n];
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0; j < n; j++){
+                paper[i][j] = Integer.parseInt(st.nextToken());
+            }
+            
+        }
+        
+        cut(0,0,n);
+
+        System.out.println(white);
+        System.out.println(blue);
+
+    }//psvm
+    
+}//class
