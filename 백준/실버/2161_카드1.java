a37fbf4fe59adbef08346c6f0991eabf63e02bcf
Seok_Soo <whdtn7855@naver.com>
2026-05-09 23:27:40 +0900

    chore: 폴더 이동

--- 백준/실버/2161_카드1.java
+++ 백준/실버/2161_카드1.java
@@ -1,3 +1,4 @@
+package 실버;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
@@ -5,15 +6,12 @@ import java.util.Queue;
 
 class Main {
 
-    static int arr[];
     static Queue<Integer> queue = new ArrayDeque<>();
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-
-        arr = new int[n-1];
 
         cards(n);
 
