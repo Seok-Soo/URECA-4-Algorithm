b4f3bcd942365a4dac1db321b33c12d9b304ab91
Seok_Soo <whdtn7855@naver.com>
2026-05-15 10:50:54 +0900

    feat: 155651_호텔대실 문제 재풀이 추가

---
+++ 프로그래머스/155651_호텔대실_재풀이.java
@@ -0,0 +1,38 @@
+package 프로그래머스;
+
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(String[][] book_time) {
+        int answer = 0;
+
+        int n = book_time.length;
+
+        //시작시간 기준 정렬
+        Arrays.sort(book_time, (o1, o2) -> {
+            return o1[0].compareTo(o2[0]);
+        });
+ 
+        // 입실, 퇴실 시간 분리
+        LocalTime enter[] = new LocalTime[n];
+        LocalTime exit[] = new LocalTime[n];
+        // 값 입력
+        for(int i = 0; i < n; i++){
+            enter[i] = LocalTime.parse(book_time[i][0]); 
+            exit[i] = LocalTime.parse(book_time[i][1]);
+        }
+
+        PriorityQueue<LocalTime> pq = new PriorityQueue<>();
+
+        for(int i = 0; i < n; i++){
+
+               pq.offer(exit[i]);
+        }
+
+
+
+        return answer;
+    }
+}
